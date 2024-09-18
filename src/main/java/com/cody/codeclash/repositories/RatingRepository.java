b6f20e63d894544ee863d6bceae7fc3a1cd77221
Ubuntu <ubuntu@ip-172-31-30-108.ec2.internal>
2024-09-18 07:34:33 +0000

    implemented rating service

--- src/main/java/com/cody/codeclash/repositories/RatingRepository.java
+++ src/main/java/com/cody/codeclash/repositories/RatingRepository.java
@@ -1,8 +1,17 @@
 package com.cody.codeclash.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RatingRepository {
+import com.cody.codeclash.entities.Problem;
+import com.cody.codeclash.entities.Rating;
 
+@Repository
+public interface RatingRepository  extends JpaRepository<Rating, Long> {
+
+    List<Rating> findAllByProblem(Problem problem);
+
+    Rating findByProblemAndUserId(Problem problem, Long userId);
 }
--- src/main/java/com/cody/codeclash/services/RatingService.java
+++ src/main/java/com/cody/codeclash/services/RatingService.java
@@ -2,8 +2,8 @@ package com.cody.codeclash.services;
 
 import org.springframework.stereotype.Service;
 
-import com.cody.codeclash.entities.Problem;
 import com.cody.codeclash.entities.Rating;
+import com.cody.codeclash.repositories.RatingRepository;
 import com.cody.codeclash.utils.UtilityFunctions;
 
 import lombok.RequiredArgsConstructor;
@@ -16,14 +16,31 @@ public class RatingService {
 
     private final ProblemService problemService;
 
-
     public void save(Long problemId, int rating) {
-        Problem problem = problemService.getById(problemId);
-        Rating ratingEntity = new Rating();
-        ratingEntity.setProblem(Problem.builder().id(problemId).build());
-        ratingEntity.setScore(rating);
-        ratingEntity.setUserId(UtilityFunctions.getUserDto().id());
+        var problem = problemService.getById(problemId);
+        var ratingEntity = Rating.builder()
+                .score(rating)
+                .problem(problem)
+                .userId(UtilityFunctions.getUserDto().id())
+                .build();
         repository.save(ratingEntity);
     }
-        
+
+    public int getAvgUserRatingOnAProblem(Long problemId) {
+        var problem = problemService.getById(problemId);
+        var ratings = repository.findAllByProblem(problem);
+        return ratings.stream()
+                .mapToInt(Rating::getScore)
+                .sum() / ratings.size();
+    }
+
+    public Rating getUserRatingOnProblem(Long problemId) {
+        return repository.findByProblemAndUserId(problemService.getById(problemId), UtilityFunctions.getUserDto().id())
+                .orElseThrow(() -> new EntityNotFoundException("Rating not found"));
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
 }
